37fe43d51d93b306bb5114c016f67e8d7d545746
FrankAnconeyra <143462191+FrankAnconeyra@users.noreply.github.com>
2023-09-17 22:30:49 -0500

    Add files via upload

---
+++ Lab03/Ejercicio06.js
@@ -0,0 +1,10 @@
+let nombreCompleto1 = "Juan Perez";
+let nombreCompleto2 = "Carlos Lara";
+console.log(nombreCompleto1);
+console.log(nombreCompleto2);
+
+let alnombrecompleto;
+let nombreCompleto;
+let $nombreCompleto;
+
+let ruptura = 10;
